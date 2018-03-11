59ef9bd8d105a7a2e83192cf985ffdccd5d8b44a
Abdel Mendez <denysabdel@gmail.com>
2018-03-11 18:05:29 -0500

    Test

--- app/src/main/java/app/qk/teliver/utils/Api.java
+++ app/src/main/java/app/qk/teliver/utils/Api.java
@@ -90,6 +90,7 @@ public class Api {
 
         stringRequest.setRetryPolicy(new DefaultRetryPolicy(
                 30000,
+
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
